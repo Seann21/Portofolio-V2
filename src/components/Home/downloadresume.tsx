cfac0f6786836bc0b3847d33d5f7e7395db1492f
Moh. Hasan efendi <109188837+Seann21@users.noreply.github.com>
2024-05-29 17:20:17 +0700

    Update colors

--- src/components/Home/downloadresume.tsx
+++ src/components/Home/downloadresume.tsx
@@ -5,7 +5,7 @@ const DownloadResumeButton = () => {
 			download='Hasanefendi CV.pdf'
 			target='_blank'
 			rel='noreferrer'
-			className='flex items-center py-2 px-4 mx-auto mt-4 text-sm font-bold bg-gradient-to-r from-pink-700 to-indigo-700 rounded-lg border md:py-2.5 md:px-5 md:mx-0 focus:outline-none w-fit text-white '
+			className='flex items-center py-2 px-4 mx-auto mt-4 text-sm font-bold bg-gradient-to-r from-pink-700 to-indigo-700 hover:from-black hover:to-violet-700 rounded-lg border md:py-2.5 md:px-5 md:mx-0 focus:outline-none w-fit text-white transition duration-300'
 			data-umami-event='Download resume button'
 		>
 			<svg
