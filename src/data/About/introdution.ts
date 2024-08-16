9fb0f1d120426b11f83e1062d28976314fa9259b
Moh. Hasan efendi <109188837+Seann21@users.noreply.github.com>
2024-08-16 11:00:00 +0700

    fix about

--- src/data/About/introdution.ts
+++ src/data/About/introdution.ts
@@ -1,8 +1,8 @@
 export const introduction: string[] = [
-  "Hi everyone, my name is Moh Hasan Efendi. I'm a Front End Developer Enthusiast, Information Systems Student, Free and Open Source Software Enthusiast",
-  `I live in Kediri, Indonesia. I was born in Kediri, 28 November 2003. I'm ${calculateAge(
-    new Date('2003-11-23')
-  )} years old. I'm a Information Systems Student from Nusantara University PGRI  Kediri.`,
+  "Hi everyone, my name is Moh Hasan Efendi. I'm a Front End Developer Enthusiast, Information Systems Student, and Free and Open Source Software Enthusiast",
+  `I live in Kediri, Indonesia. I was born in Kediri on November 28, 2003. I'm ${calculateAge(
+    new Date('2003-11-28')
+  )} years old. I'm an Information Systems Student at Nusantara University PGRI Kediri.`,
   'My favorite programming language is TypeScript, and I enjoy working with React.js or Next.js.',
   'My interest in technology stems from curiosity about innovations and how popular websites function. Aspiring to become a Software Engineer, I'm committed to developing my skills and contributing to the tech world.',
 ];
