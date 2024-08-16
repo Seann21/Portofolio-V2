6fdf1c1b5c0929eaaba4830e4311ceebcc6f779b
Moh. Hasan efendi <109188837+Seann21@users.noreply.github.com>
2024-08-16 10:57:43 +0700

    Update about

--- src/data/About/introdution.ts
+++ src/data/About/introdution.ts
@@ -3,8 +3,8 @@ export const introduction: string[] = [
   `I live in Kediri, Indonesia. I was born in Kediri, 28 November 2003. I'm ${calculateAge(
     new Date('2003-11-23')
   )} years old. I'm a Information Systems Student from Nusantara University PGRI  Kediri.`,
-  'I am someone who started loving coding at the age of 18. My learning journey started with HTML and C++, and the first programming language I mastered was C++. My great interest in the world of technology grew from curiosity about the latest innovations. Since I was little, I enjoyed visiting icafe/internet cafes, but my understanding of the world of computers developed when I realized the crucial role of software. and Always want to explore the latest technology',
-  'My interests focus on understanding how websites like Facebook, Twitter, and Instagram work. My dream is to become a Frontend Developer.I have started developing programming skills and am willing to continue learning to achieve this goal. With an educational background in information technology, I am committed to continuing to develop myself and make contributions to the world of technology.',
+  'My favorite programming language is TypeScript, and I enjoy working with React.js or Next.js.',
+  'My interest in technology stems from curiosity about innovations and how popular websites function. Aspiring to become a Software Engineer, I'm committed to developing my skills and contributing to the tech world.',
 ];
 
 function calculateAge(birthday: Date) {
